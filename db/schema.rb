e3cca661bedff7b6004ce9922fef334e5b878ef2
Romane Steisel <romane.steisel@hotmail.com>
2018-11-07 13:36:05 +0100

    frontend almost done on the show viex of a granpa

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_06_154014) do
+ActiveRecord::Schema.define(version: 2018_11_07_104925) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 2018_11_06_154014) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "photo"
+    t.text "description"
     t.index ["user_id"], name: "index_grandparents_on_user_id"
   end
 
