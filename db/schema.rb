b1f4b68d2f74284367a52c2b73b6657dde025da7
viktor weinand <weinand.viktor@gmail.com>
2018-11-07 18:00:33 +0100

    test-map still needs to update api key

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2018_11_07_141551) do
-
-
-
-
+ActiveRecord::Schema.define(version: 2018_11_07_152724) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +38,8 @@ ActiveRecord::Schema.define(version: 2018_11_07_141551) do
     t.datetime "updated_at", null: false
     t.string "picture"
     t.text "description"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_grandparents_on_user_id"
   end
 
