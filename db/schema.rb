a3220d88206d138d9a5c8de97ad5b3bb917698c4
Romane Steisel <romane.steisel@hotmail.com>
2018-11-08 10:27:06 +0100

    last changes staged for commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2018_11_07_141551) do
-
-
-
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,8 +36,8 @@ ActiveRecord::Schema.define(version: 2018_11_07_141551) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "picture"
     t.text "description"
+    t.string "picture"
     t.index ["user_id"], name: "index_grandparents_on_user_id"
   end
 
