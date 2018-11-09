c5dab540f335b932d3ca66d564685c784fe459bd
Francois Kiesecoms <kiesecoms.francois@gmail.com>
2018-11-09 09:40:25 +0100

    fixed calendars

--- db/schema.rb
+++ db/schema.rb
@@ -36,12 +36,12 @@ ActiveRecord::Schema.define(version: 2018_11_08_115010) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.text "description"
     t.string "picture"
-    t.float "latitude"
-    t.float "longitude"
+    t.text "description"
     t.datetime "start_date"
     t.datetime "end_date"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_grandparents_on_user_id"
   end
 
